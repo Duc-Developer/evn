fa54d5df126adc143e5af98f14cf56632d4a5871
David Chan <mrtranduc1994@outlook.com>
2025-02-07 00:07:01 +0700

    add logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,15 @@
 import './App.css';
-import { useEffect } from 'react';
 import { observer } from 'mobx-react-lite';
+import evnIconSrc from '@assets/icons/evn.svg';
 
 import commonStore from '@src/Stores/commonStore';
 
 const App = observer(() => {
     const store = new commonStore();
 
-    const drawBitcoin = () => {
-        const canvas: HTMLCanvasElement = document.getElementById('bitcoinCanvas') as HTMLCanvasElement;
-        const ctx: CanvasRenderingContext2D = canvas.getContext('2d')!;
-
-        // Draw circle
-        ctx.beginPath();
-        ctx.arc(100, 100, 80, 0, 2 * Math.PI);
-        ctx.fillStyle = 'orange';
-        ctx.fill();
-
-        // Draw inner circle
-        ctx.beginPath();
-        ctx.arc(100, 100, 60, 0, 2 * Math.PI);
-        ctx.fillStyle = 'white';
-        ctx.fill();
-
-        // Draw "₿"
-        ctx.font = 'bold 80px Arial';
-        ctx.fillStyle = 'orange';
-        ctx.fillText('₿', 75, 135);
-
-        // Draw "₿"
-        ctx.font = 'bold 80px Arial';
-        ctx.fillStyle = 'white';
-        ctx.fillText('₿', 95, 135);
-    };
-    useEffect(() => {
-        drawBitcoin();
-    }, []);
-
-    useEffect(() => {
-        console.log('%cAttention', 'color: red; font-weight: bold; font-size:3rem');
-        console.log(
-            `%cVisit %chttp://localhost:3000/README.md to read project's conventions`,
-            'font-weight: bold;',
-            'font-weight: bold;',
-        );
-    }, []);
     return (
         <>
-            <canvas id="bitcoinCanvas" width="200" height="200"></canvas>
+            <img src={evnIconSrc} width={232} height={232} alt="logo" />
             <h1 className="text-blue-700">{store.appName}</h1>
             <p className="descriptions">My app based on react and vite</p>
             <ul className="packages">
