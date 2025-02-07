749bd821f8ac0a747207fb9df32056905641396b
David Chan <mrtranduc1994@outlook.com>
2025-02-07 22:22:47 +0700

    add primereact

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,18 @@
 import './App.css';
 import { observer } from 'mobx-react-lite';
 import evnIconSrc from '@assets/icons/evn.svg';
-
+import { Button } from 'primereact/button';
 import commonStore from '@src/Stores/commonStore';
 
 const App = observer(() => {
     const store = new commonStore();
 
     return (
-        <>
+        <div className="flex flex-col items-center justify-center h-screen">
             <img src={evnIconSrc} width={232} height={232} alt="logo" />
             <h1 className="text-blue-700">{store.appName}</h1>
-            <p className="descriptions">My app based on react and vite</p>
-            <ul className="packages">
-                <li>axios</li>
-                <li>mobx</li>
-                <li>i18next</li>
-            </ul>
-        </>
+            <Button label="Check" icon="pi pi-check" />
+        </div>
     );
 });
 
