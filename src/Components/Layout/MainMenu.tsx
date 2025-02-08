4bbc3b3100d3a2a203bb209b7d017dcf4aefa3ee
David Chan <mrtranduc1994@outlook.com>
2025-02-08 17:33:56 +0700

    update menu and font

--- src/Components/Layout/MainMenu.tsx
+++ src/Components/Layout/MainMenu.tsx
@@ -13,63 +13,18 @@ const MainMenu = () => {
             },
         },
         {
-            label: 'Users',
-            icon: 'pi pi-fw pi-users',
+            label: 'Công việc',
+            icon: 'pi pi-fw pi-briefcase',
             items: [
                 [
                     {
-                        label: 'User 1',
-                        items: [{ label: 'User 1.1' }, { label: 'User 1.2' }],
+                        label: 'Giỏ công việc',
                     },
                     {
-                        label: 'User 2',
-                        items: [{ label: 'User 2.1' }, { label: 'User 2.2' }],
-                    },
-                ],
-                [
-                    {
-                        label: 'User 3',
-                        items: [{ label: 'User 3.1' }, { label: 'User 3.2' }],
+                        label: 'Quản lý phân việc',
                     },
                     {
-                        label: 'User 4',
-                        items: [{ label: 'User 4.1' }, { label: 'User 4.2' }],
-                    },
-                ],
-                [
-                    {
-                        label: 'User 5',
-                        items: [{ label: 'User 5.1' }, { label: 'User 5.2' }],
-                    },
-                    {
-                        label: 'User 6',
-                        items: [{ label: 'User 6.1' }, { label: 'User 6.2' }],
-                    },
-                ],
-            ],
-        },
-        {
-            label: 'Events',
-            icon: 'pi pi-fw pi-calendar',
-            items: [
-                [
-                    {
-                        label: 'Event 1',
-                        items: [{ label: 'Event 1.1' }, { label: 'Event 1.2' }],
-                    },
-                    {
-                        label: 'Event 2',
-                        items: [{ label: 'Event 2.1' }, { label: 'Event 2.2' }],
-                    },
-                ],
-                [
-                    {
-                        label: 'Event 3',
-                        items: [{ label: 'Event 3.1' }, { label: 'Event 3.2' }],
-                    },
-                    {
-                        label: 'Event 4',
-                        items: [{ label: 'Event 4.1' }, { label: 'Event 4.2' }],
+                        label: 'Theo dõi tiến độ',
                     },
                 ],
             ],
